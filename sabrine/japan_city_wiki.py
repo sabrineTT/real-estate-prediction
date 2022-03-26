e9c39e5bb5f25e02632037fab4d9f66511395242
sabrineTT <sabrine.thibert@esme.fr>
2022-03-26 12:18:56 +0100

    création du dataFrame et d'un tableau CSV avec les données du tableau HTML

--- sabrine/japan_city_wiki.py
+++ sabrine/japan_city_wiki.py
@@ -3,36 +3,52 @@ import pandas as pd
 from bs4 import BeautifulSoup
 
 wiki_url = 'https://fr.wikipedia.org/wiki/Liste_des_villes_du_Japon_par_nombre_d%27habitants'
-html_text = requests.get(wiki_url).text
-soup = BeautifulSoup(html_text, 'html.parser')
 
-# On créer une liste vides qui contiendra les datas des villes
-data_villes = []
+def html_data_to_csv(url, csv_name):
+    """
+    Récupère les données d'un tableau HTML d'une page web
+    et les stocks dans un dataFrame pandas pour
+    ensuite le convertir en tableau CSV
 
-# On récupère les en-têtes du tableau HTML
-list_header = []
-header = soup.find_all("table")[0].find("tr")
+    :param str url: le lien de la page web
+    :param str csv_name: le nom du tableau csv qu'on l'on obtiendra a la fin
 
-for items in header:
-    try:
-        list_header.append(items.get_text())
-    except:
-        continue
+    :return: un tableau CSV
+    :rtype: CSV
+    """
+    # On récupère le fichier html du lien voulu
+    html_text = requests.get(url).text
+    soup = BeautifulSoup(html_text, 'html.parser')
 
-# On récupère les données du tableau HTML
-HTML_data = soup.find_all("table")[0].find_all("tr")[1:]
+    # On créer une liste vides qui contiendra les datas des villes
+    data = []
 
-for element in HTML_data:
-    sub_data = []
-    for sub_element in element:
+    # On récupère les en-têtes du tableau HTML
+    list_header = []
+    header = soup.find_all("table")[0].find("tr")
+
+    for items in header:
         try:
-            sub_data.append(sub_element.get_text())
+            list_header.append(items.get_text())
         except:
             continue
-    data_villes.append(sub_data)
 
-# On sauvegarde les données du tableau HTML dans un dataFrame pandas
-tableau_donnees_villes = pd.DataFrame(data=data_villes, columns=list_header)
+    # On récupère les données du tableau HTML
+    HTML_data = soup.find_all("table")[0].find_all("tr")[1:]
 
-# Et on converti le dataFrame en fichier CSV
-tableau_donnees_villes.to_csv('villes_japon.csv')
+    for element in HTML_data:
+        sub_data = []
+        for sub_element in element:
+            try:
+                sub_data.append(sub_element.get_text())
+            except:
+                continue
+        data.append(sub_data)
+
+    # On sauvegarde les données du tableau HTML dans un dataFrame pandas
+    tableau_donnees = pd.DataFrame(data=data, columns=list_header)
+
+    # Et on converti le dataFrame en fichier CSV
+    return (tableau_donnees.to_csv(csv_name+'.csv'))
+
+html_data_to_csv(wiki_url, "villes_japon")
