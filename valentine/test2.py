811dc4b6bcaa79be0727e04c6271d15afe8f645b
valentinesallet <valentine.sallet-bernard@esme.fr>
2022-04-15 14:49:53 +0200

    scrapping selenium ajout commentaire pour comprendre

---
+++ valentine/test2.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 26 12:00:13 2022
+
+@author: kju78
+"""
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import pandas as pd
+import time
+import re
+
+options = Options()
+options.headless = True  # etre prive
+options.add_argument("--window-size=1920,1080")  # dimension fenetre
+options.add_argument("start-maximized")  # mise en plein ecran de la fenetre
+
+driver = webdriver.Chrome("/Users/valentinesallet/Desktop/pourEssayer/chromedriver")  # adresse driver chrome
+
+# =============================================================================
+# Variables a definir :
+# =============================================================================
+
+page = 1  # variable pour indentation nombre de page parcourues
+debut = 1
+fin = 5
+
+# =============================================================================
+
+datas_list = []  # listes vides pour stocker datas scrapees
+prices_list = []
+nombre_pieces_list = []
+superficie_list = []
+clean_prices_list = []
+arrondiessements_list = []
+
+# =============================================================================
+# Scraping :
+# =============================================================================
+
+while debut <= page <= fin:  # boucle pour 5 pages differentes
+
+    url = 'https://www.logic-immo.com/vente-immobilier-paris-75,100_1/options/groupprptypesids=1/page=%d'%(page)  # adresse de la page scrappee
+    driver.get(url)  # ouverture avec driver
+
+    if page == debut:
+        time.sleep(12)  # pause
+    else:
+        time.sleep(3)
+
+    datas = driver.find_elements_by_xpath('//a[@class="linkToFa"]')  # recupere tous les elements avec une borne a et une classe "linktofa"
+    prices = driver.find_elements_by_xpath('//span[@class="announceDtlPrice"]')
+    arrondiessements = driver.find_elements_by_xpath('//div[@class="announcePropertyLocation"]')
+
+    page += 1
+
+    for d in range(len(datas)):
+        datas_list.append(datas[d].text)  # ajoute chaque element recupere au dessus dans une liste defenie avant
+
+    for p in range(len(prices)):
+        prices_list.append(prices[p].text)
+
+    for a in range(len(arrondiessements)):
+        arrondiessements_list.append(arrondiessements[a].text)
+
+driver.close()  # fermeture de la fenetre de scraping
+
+# =============================================================================
+# Nettoyage des données :
+# =============================================================================
+
+for data in datas_list:
+    digit = re.findall(r"\d", data)  # recupere uniquement les chiffres de data
+
+    nombre_pieces = digit[-1] + "p"  # le dernier chiffre correspond au nombre de pieces
+    nombre_pieces_list.append(nombre_pieces)
+    del digit[-1]  # retire le nombre de piece de data
+
+    superficie = ("".join(digit)) + "m2"  # concatene les chiffres restant dans data sans separateur
+    superficie_list.append(superficie)
+
+for price in prices_list:
+    clean_data = re.sub("\€", "", price) + "Euros"  # supprime le signe €
+    clean_prices_list.append(clean_data)
+
+df = pd.DataFrame(list(zip(superficie_list, nombre_pieces_list, clean_prices_list, arrondiessements_list)),columns=['Superficie', 'Nombre Piece', 'Prix', 'Code Postal'])  # creation d'une base de donnees
+print(df)  # affichage de la base
+df.to_csv(r'C:\Users\kju78\Documents\ESME Sudria\Ingé 2\ESME Sudria - Ingé 2\Projet - Machine Learning Immobilier\Scraping\logicimmo.csv',index=False)  # converti base pandas en fichier csv
+
+
+
+
--- valentine/wikipedia.py
+++ valentine/wikipedia.py
@@ -24,36 +24,39 @@ Logic_url = 'https://www.logic-immo.com/vente-immobilier-paris-75,100_1/options/
 html_text = requests.get(Logic_url).text
 soup = BeautifulSoup(html_text, 'html.parser')
 
-print(soup) # ca print le code html dans la console
+# print(soup) # ca print le code html dans la console
 
 Annonces = []  # on creer une liste annonces qui va lister toutes les annonces
 
 table = soup.find('div', attrs={"id": "lists-offer"})  # on recupere la div qui a l'id list-offer
-print(table)
+# print(table)
 
-for row in table.findAll('div', attrs={'class': 'announceContent'}):  #on cherche toute les balises qui ont announcecontent et on va faire pour chaque element de la liste :
+for row in table.find_all('div', attrs={'class': 'announceContent'}):  #on cherche toute les balises qui ont announcecontent et on va faire pour chaque element de la liste :
 
     annonce = {}   # on crée un dictionnaire, ca servira pour chaque annonce a lister le prix la surface etc
 
     x1 = row.findChildren("span", attrs={"class": "announceDtlPrice"}) # on cherche dans les "enfants" de l'élément les span de class announceDtlPrice
     for i in x1:
-        annonce['Prix'] = i.get_text(separator="  #") # on recupere le texte de la balises span et on met dans le dictionnaire
+        annonce['Prix'] = " ".join(i.get_text().split()) # on recupere le texte de la balises span et on met dans le dictionnaire
+
     x2 = row.findChildren("div", attrs={"class": "announcePropertyLocation"})
     for i in x2:
-        annonce['Location'] = i.text
+        annonce['Location'] = " ".join(i.get_text().split())
+
     x3 = row.findChildren("span", attrs={"class": "announceDtlInfosPropertyType"})
     for i in x3:
-        annonce['Type'] = i.text
+        annonce['Type'] = " ".join(i.get_text().split())
     x4 = row.findChildren("span", attrs={"class": "announceDtlInfos announceDtlInfosArea"})
-    for i in x1:
-        annonce['Area'] = i.text
+    for i in x4:
+        annonce['Area'] = " ".join(i.get_text().split())
     x5 = row.findChildren("span", attrs={"class": "announceDtlInfos announceDtlInfosNbRooms"})
-    for i in x1:
-        annonce['NbRoom'] = i.text
+    for i in x5:
+        annonce['NbRoom'] = " ".join(i.get_text().split())
     Annonces.append(annonce) # on ajoute le dictionnaire a la liste
 # il y a autant de dictionnaire que de liste.
 
-print(Annonces) on print la liste
+print(Annonces) #on print la liste
+print(len(Annonces))
 
 '''
 Probleme 1 :
